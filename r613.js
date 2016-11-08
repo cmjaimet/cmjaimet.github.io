a0b3218523053e1df154507f5744dbd15495d571
Charles Jaimet <cjaimet@postmedia.com>
2016-11-08 16:16:03 -0500

    JS Refugee613

--- r613.js
+++ r613.js
@@ -1,1 +1,36 @@
 console.log(' Time to deliver a pizza ball!');
+$.ajax({
+  url: "http://r613.calmseamedia.com/cmj.php",
+  dataType: "jsonp",
+  jsonpCallback: "r613_events_cb"
+});
+function r613_events_cb( data_ ) {
+  console.log('ok r613_events_cb');
+	$.each( data_, function( _index, _value ) {
+		_wrapper = document.getElementById( 'r613_events' );
+		_event = document.createElement( 'div' );
+		_title = document.createElement( 'h2' );
+		_title.innerHTML = _value.title;
+		_date = document.createElement( 'p' );
+		_date.innerHTML = 'Date: ' + _value.date + ' ' + _value.time;
+		_location = document.createElement( 'p' );
+		_location.innerHTML = 'Location: ' + _value.location;
+		_details = document.createElement( 'p' );
+		_details.innerHTML = _value.details;
+		_link = document.createElement( 'a' );
+		_link.innerHTML = 'More...';
+		_link.href = _value.link;
+		_image = document.createElement( 'img' );
+		_image.src = _value.image;
+		_image.style.width = '200px';
+		_image.style.float = 'right';
+		_event.appendChild( _image );
+		_event.appendChild( _title );
+		_event.appendChild( _date );
+		_event.appendChild( _location );
+		_event.appendChild( _details );
+		_event.appendChild( _link );
+		_wrapper.appendChild( _event );
+	   // console.log(value);
+	});
+}
